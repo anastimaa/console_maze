93e307bf58e6a154a3125f476eba2d355e45a62c
linwh06 <p.v.alexeeva2006@gmail.com>
2024-12-16 15:27:25 +0300

    Тесты к моей части проекта (Полина)

--- client_test.py
+++ client_test.py
@@ -1,11 +1,10 @@
-import socket
 import pytest
-import pickle
-from unittest.mock import Mock, patch
+from unittest.mock import patch
 from server import generate_maze, game_state
 from dfsmaze import dfsmaze_generate, dfs
 from сlient import game_menu, print_game_state, receive_data
-from io import StringIO
+
+# тест функции dfsmaze_generate
 
 def test_dfsmaze_generate_correct():
     maze = dfsmaze_generate(7, 7)
@@ -29,6 +28,8 @@ def test_test_dfsmaze_generate_invalid_height():
     with pytest.raises(ValueError):
         dfsmaze_generate(1, 7)
 
+# тест dfs
+
 def test_dfs_invalid_starting_point():
     maze = [["\u2588" for _ in range(5)] for _ in range(5)]
     with pytest.raises(IndexError):
@@ -39,6 +40,8 @@ def test_dfs_start_position_not_empty():
     maze[1][1] = "\u2588"
     with pytest.raises(ValueError):
         dfs(maze, 7, 7, 1, 1)
+
+# тест generate_maze
 
 def test_generate_maze_level_1():
     maze = generate_maze(1)
@@ -86,13 +89,6 @@ def test_items_generation():
     assert gems >= 3
 
 
-def test_mob_positions():
-    generate_maze(2)
-    for mb in game_state["mobs"]:
-        assert 0 < mb["x"] < 20
-        assert 0 < mb["y"] < 15
-
-
 def test_generate_maze_invalid_level_0():
     with pytest.raises(ValueError):
         generate_maze(0)
@@ -124,51 +120,3 @@ def test_new_game_valid_level():
     with patch('builtins.input', side_effect = ["N", "2"]):
         level = game_menu()
         assert level == 2
-
-# тест для print_game_state
-
-@pytest.fixture
-def mock_socket():
-    mock_conn = Mock()
-    mock_conn.recv = Mock()
-    mock_conn.sendall = Mock()
-    mock_conn.close = Mock()
-    return mock_conn
-
-@patch("sys.stdout", new_callable=StringIO)
-def test_print_game_state_empty_maze(mock_stdout):
-    # Тест с пустым лабиринтом
-    state_empty_maze = {
-        "maze": [],
-        "players": {1: (0, 0)},
-        "codegame": 12,
-        "message": ""
-    }
-    print_game_state(state_empty_maze)
-    output = mock_stdout.getvalue()
-
-    # Проверки, что вывод содержит только информацию о игроках
-    assert "Player 1: (0, 0)" in output
-    assert "Code the game: 12" in output
-    assert "Hello" not in output
-
-# тест для receive_data
-
-@patch("sys.stdout", new_callable=StringIO)
-def test_receive_data(mock_stdout,mock_socket):
-    game_state = {
-    "maze": None, "players": {1: {"x": 1, "y": 1, "lives":3, "keys": 0,"gems":0}, 2: {"x": 1, "y": 1, "lives":3,"keys": 0,"gems":0}},  # Позиции игроков
-    "items": [], "mobs": None,
-    "level":0, "codegame":20,
-    "message":""}
-    mock_socket.recv.side_effect = [pickle.dumps(game_state), b'']
-    receive_data(mock_socket)
-    output = mock_stdout.getvalue()
-    assert 'Code the game: 20' in output
-
-@patch("sys.stdout", new_callable=StringIO) # если не пришли данные
-def test_receive_nodata(mock_stdout,mock_socket):
-    mock_socket.recv.side_effect = [None, b'']
-    receive_data(mock_socket)
-    output = mock_stdout.getvalue()
-    assert 'Disconnected from server.' in output
